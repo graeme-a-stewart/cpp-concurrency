ab0ab6aa19092b3f9db42a23e5fd672c50be3cfc
Graeme A Stewart <graeme.a.stewart@gmail.com>
2014-09-04 10:30:57 +0200

    Aquire locks in the same order, using std::lock

---
+++ src/cpp11/deadlock-fixed.cc
@@ -0,0 +1,46 @@
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+std::mutex kettle, tap;
+
+#define THREAD_POOL 8
+
+void kettle_tap() {
+  std::cout << "Locking kettle and tap in " << std::this_thread::get_id() << std::endl;
+  std::lock(kettle, tap);
+  std::lock_guard<std::mutex> kettle_lock(kettle, std::adopt_lock);
+  std::lock_guard<std::mutex> tap_lock(tap, std::adopt_lock);
+
+  std::cout << "Locked kettle and tap in " << std::this_thread::get_id() << std::endl;
+  
+  std::cout << "Filling kettle in " << std::this_thread::get_id() << std::endl;
+}
+
+void tap_kettle() {
+  std::cout << "Locking tap and kettle in " << std::this_thread::get_id() << std::endl;
+  std::lock(kettle, tap);
+  std::lock_guard<std::mutex> tap_lock(tap, std::adopt_lock);
+  std::lock_guard<std::mutex> kettle_lock(kettle, std::adopt_lock);
+
+  std::cout << "Locked tap and kettle in " << std::this_thread::get_id() << std::endl;
+  
+  std::cout << "Filling kettle in " << std::this_thread::get_id() << std::endl;
+}
+
+
+int main() {
+  std::thread pool[THREAD_POOL];
+
+  for (int t=0; t<THREAD_POOL; t+=2) {
+    pool[t] = std::thread(kettle_tap);
+    pool[t+1] = std::thread(tap_kettle);
+  }
+
+  for (int t=0; t<THREAD_POOL; ++t)
+    pool[t].join();
+
+  std::cout << "Threads are all joined" << std::endl;
+
+  return 0;
+}
