722352b2833f90e6a9b9913a381df82e85ab99c1
Graeme A Stewart <graeme.a.stewart@gmail.com>
2014-09-04 09:37:08 +0200

    Fixed parallel_mandel:       Moved to using std::complex to make logic clerer.       Print a map of the set at the end.

--- src/tbb/parallel_mandel.cc
+++ src/tbb/parallel_mandel.cc
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <vector>
+#include <complex>
 #include <tbb/tbb.h>
 
 const unsigned int max_iter=256;
-const size_t res=100;
+const size_t res=150;
 
 class parallel_mandel {
 private:
@@ -13,23 +14,22 @@ public:
   void operator()(tbb::blocked_range2d<size_t>& r) const {
     bool (*set)[res] = my_set;
     for (size_t i=r.rows().begin(); i!=r.rows().end(); ++i) {
-      for (size_t j=r.cols().begin(); j!=r.cols().begin(); ++j) {
-	double x0 = double(i)/res * 2.0 - 1.0;	
-	double y0 = double(j)/res * 2.0 - 1.0;
-	double x=x0;
-	double y=y0;
-	double last_x=x;
-	double last_y=y;
+      for (size_t j=r.cols().begin(); j!=r.cols().end(); ++j) {
+	std::complex<float> z0(double(i)/res * 4.0 - 2.0, double(j)/res * 4.0 - 2.0);
+#ifdef DEBUG
+	std::cout << "Trying " << z0.real() << "," << z0.imag() << " ";
+#endif
+	std::complex<float> z(0.0, 0.0);
 	unsigned int iter=0;
 	bool inside=true;
 	while (inside && (++iter < max_iter)) {
-	  x = last_x*last_x - last_y*last_y + x0;
-	  y = 2*last_x*last_y + y0;
-	  last_x = x;
-	  last_y = y;
-	  if (x*x + y*y > 4.0)
+	  z = z*z + z0;
+	  if (std::abs(z) > 2.0)
 	    inside=false;
 	}
+#ifdef DEBUG
+	std::cout << inside << std::endl;
+#endif
 	set[i][j] = inside;
       }
     }
@@ -44,6 +44,20 @@ int main() {
   bool set[res][res];
 
   tbb::parallel_for(tbb::blocked_range2d<size_t>(0, res, 0, res), parallel_mandel(set));
+
+  // Now print the map of points
+  if (res <= 150) {
+    for (size_t i=0; i<res; ++i) {
+      for (size_t j=0; j<res; ++j) {
+	if (set[j][i])
+	  std::cout << " ";
+	else
+	  std::cout << ".";
+      }
+      std::cout << std::endl;
+    }
+  }
+    
 
   return 0;
 }
