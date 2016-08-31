4b4df8d51c36a2cd268ea9ccacecabcccb95fd00
Graeme A Stewart <graeme.andrew.stewart@cern.ch>
2016-08-31 11:23:48 +0200

    Examples showing 'tee' connections in the DAG

--- src/tbb/graph/dep_graph_broadcast.cc
+++ src/tbb/graph/dep_graph_broadcast.cc
@@ -1,4 +1,4 @@
-// TBB Graph example showing how to use multiple edges
+// TBB Graph example showing how to use broadcast nodes
 // to make 'tee' pipe connections in the DAG
 
 #include <iostream>
@@ -7,15 +7,16 @@
 #include "tbb/flow_graph.h"
 
 typedef tbb::flow::function_node< tbb::flow::continue_msg, tbb::flow::continue_msg > node_t;
+typedef tbb::flow::broadcast_node< tbb::flow::continue_msg > tee_t;
 typedef const tbb::flow::continue_msg & msg_t;
 
 int main() {
   tbb::flow::graph g;
   node_t A(g, 1, [](msg_t m){ std::cout << "A" << std::endl; return m; } );
-  node_t B(g, 1, [](msg_t m){ std::cout << "B" << std::endl; return m; } );
+  tee_t  B(g);
   node_t C(g, 1, [](msg_t m){ std::cout << "C" << std::endl; return m; } );
   node_t D(g, 1, [](msg_t m){ std::cout << "D" << std::endl; return m; } );
-  node_t E(g, 1, [](msg_t m){ std::cout << "E" << std::endl; return m; } );
+  tee_t  E(g);
   node_t F(g, 1, [](msg_t m){ std::cout << "F" << std::endl; return m; } );
   make_edge(A, B);
   make_edge(B, C);
